5bdda972f075ed49f3a00da50adc0b933c0b67d2
waleedhassan304 <78468838+waleedhassan304@users.noreply.github.com>
2024-08-01 01:56:38 +0500

    customm logger added in src/__init__.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Add the src directory to the sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+from cnnClassifier import logger
+logger.info("This is my app log")
+
